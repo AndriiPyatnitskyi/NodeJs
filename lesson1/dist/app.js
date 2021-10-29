d43cf22f680cb1436f41c6646f8d0dc352ebef21
Piatnytskyi Andrii <pyatnitskiy.a@gmail.com>
2021-10-29 16:33:25 +0300

    Delete lesson1/dist directory

--- lesson1/dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("./module/user/User");
-new User_1.User("Garry");
-// console.log("hello World");
-//# sourceMappingURL=app.js.map
--- lesson1/dist/module/user/User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class User {
-    constructor(name) {
-        console.log(`User ${name} created`);
-    }
-}
-exports.User = User;
-//# sourceMappingURL=User.js.map
